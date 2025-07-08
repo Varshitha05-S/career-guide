acfc9d3c7261e8675bdfe1976fb7afea493e738c
Varshitha05-S <varshithasamiappan@gmail.com>
2025-07-08 16:44:41 +0530

    Fixed host and port for Render deployment

--- career_guide_chatbot/app.py
+++ career_guide_chatbot/app.py
@@ -207,4 +207,4 @@ def public_answers():
     return render_template("public_answers.html", messages=answered)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
