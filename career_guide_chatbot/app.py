a84d31fa452df958e3ab911c8c91cc7d21df779b
Varshitha05-S <varshithasamiappan@gmail.com>
2025-07-09 08:39:36 +0530

    Fi: Bind Flask app to 0.0.0.0 for Render

--- career_guide_chatbot/app.py
+++ career_guide_chatbot/app.py
@@ -211,4 +211,5 @@ def govtjobs(): return render_template("govtjobs.html")
 def engineering(): return render_template("engineering.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+
